5c70d9ddad8a77d1807ce760d613edbb7eb82d81
konstantin_maiuk <maiuk.kostiantyn@gmail.com>
2019-02-26 22:45:02 +0200

    added logging for possesed player controller

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "Tank.h"
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h"
@@ -13,8 +14,9 @@ UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
-	
-	
+
+public:
+	ATank* GetControlledTank() const;
+	virtual void BeginPlay() override;	
 	
 };
