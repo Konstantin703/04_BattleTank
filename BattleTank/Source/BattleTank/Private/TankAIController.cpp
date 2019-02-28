6c6f1bd617f39b8a05135afdddc89e3ea9df1eba
konstantin_maiuk <maiuk.kostiantyn@gmail.com>
2019-02-28 21:57:25 +0200

    133. Get Player Controller With C++. Created methods finding player controller for AITankController.

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -2,10 +2,43 @@
 
 
 #include "TankAIController.h"
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
 
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
+	PlayerTankDetection();
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
+}
+
+void ATankAIController::PlayerTankDetection() const
+{
+	auto PlayerTank = GetPlayerTank();
+	if (PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player controller tank detected: %s"), *PlayerTank->GetName())
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Couldn't find player controller tank "))
+	}
+}
+
+// not used yet
+void ATankAIController::AIControllerPossesion() const
+{
 	auto AIControlledTank = GetControlledTank();
 	if (AIControlledTank)
 	{
@@ -15,10 +48,5 @@ void ATankAIController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Error, TEXT("AIController not possesing a tank"));
 	}
-}
-
-ATank* ATankAIController::GetControlledTank() const
-{
-	return Cast<ATank>(GetPawn());
 }
 
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -15,8 +15,16 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
-public:
+private:
 	ATank* GetControlledTank() const;
+	
 	virtual void BeginPlay() override;
+	
+	ATank* GetPlayerTank() const;
+
+	void PlayerTankDetection() const;
+
+	// method for testing
+	void AIControllerPossesion() const;
 
 };
