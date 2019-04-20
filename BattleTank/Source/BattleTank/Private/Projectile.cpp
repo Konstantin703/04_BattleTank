3a8993c9be55ecda998924cefd57b279eec6612b
konstantin_maiuk <maiuk.kostiantyn@gmail.com>
2019-04-20 11:58:25 +0200

    212. Particle Bounding Boxes.

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Components/StaticMeshComponent.h"
 #include "Projectile.h"
+#include "Components/StaticMeshComponent.h"
 #include "Engine/World.h"
 
 // Sets default values
